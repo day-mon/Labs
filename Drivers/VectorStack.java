f9ab8dc159fe886c5d7f5de9ecee59210e3035cb
damon <dam286@pitt.edu>
2020-10-12 19:20:42 -0400

    fixed search again :(

---
+++ Drivers/VectorStack.java
@@ -0,0 +1,61 @@
+import java.util.Vector;
+import java.util.EmptyStackException;
+
+public class VectorStack<T> implements Stack<T> {
+
+    private Vector<T> stack;
+
+    public VectorStack() {
+        stack = new Vector<T>();
+    }
+
+    /**
+     * Adds something to the top of the stack.
+     * @param obj - the object to be added.
+     */
+    public T push(T obj) {
+        stack.add(obj);
+        return obj;
+    }
+
+    /**
+     * Returns a refernce to the top of the stack.
+     * Does not modify the stack.
+     * @return a reference to the top of the stack.
+     */
+    public T peek() {
+        if (empty())
+            throw new EmptyStackException();
+        return stack.get(stack.size()-1);
+    }
+
+    /**
+     * Removes the top element from the stack.
+     * @return a reference to the element that was on the top of the stack.
+     */
+    public T pop() {
+        if (empty()) {
+            throw new EmptyStackException();
+        }
+        T rem = stack.get(stack.size()-1);
+        stack.remove(stack.size()-1);
+        return rem;
+
+    }
+
+    /**
+     * Determines if the stack is empty.
+     * @return true if the stack is empty.
+     */
+    public boolean empty()
+    {
+        return stack.isEmpty();
+    }
+
+   
+    public int search(T obj) {
+        int index = stack.lastIndexOf(obj);
+        return index<0 ? -1:stack.size()-index;
+    }
+}
+
